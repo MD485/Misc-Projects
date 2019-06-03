347b9fb6395431afc68605b3c05d5228ee1cc758
Marin <md485@kent.ac.uk>
2019-06-03 07:11:31 +0100

    Added Mayan Calendar
    A project I made in 2016 during college, it converts a Julian date to the mayan equivalent.

---
+++ MayanCalendar/WindowsApplication1/MainForm.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDay = New System.Windows.Forms.TextBox()
+        Me.txtBaktun = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.txtMonth = New System.Windows.Forms.TextBox()
+        Me.txtKin = New System.Windows.Forms.TextBox()
+        Me.txtUinal = New System.Windows.Forms.TextBox()
+        Me.txtTun = New System.Windows.Forms.TextBox()
+        Me.txtKatun = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtDay
+        '
+        Me.txtDay.Location = New System.Drawing.Point(52, 10)
+        Me.txtDay.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtDay.Name = "txtDay"
+        Me.txtDay.Size = New System.Drawing.Size(76, 20)
+        Me.txtDay.TabIndex = 0
+        '
+        'txtBaktun
+        '
+        Me.txtBaktun.Enabled = False
+        Me.txtBaktun.Location = New System.Drawing.Point(9, 41)
+        Me.txtBaktun.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtBaktun.Name = "txtBaktun"
+        Me.txtBaktun.Size = New System.Drawing.Size(76, 20)
+        Me.txtBaktun.TabIndex = 1
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(290, 10)
+        Me.txtYear.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(76, 20)
+        Me.txtYear.TabIndex = 3
+        '
+        'txtMonth
+        '
+        Me.txtMonth.Location = New System.Drawing.Point(168, 10)
+        Me.txtMonth.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtMonth.Name = "txtMonth"
+        Me.txtMonth.Size = New System.Drawing.Size(76, 20)
+        Me.txtMonth.TabIndex = 2
+        '
+        'txtKin
+        '
+        Me.txtKin.Enabled = False
+        Me.txtKin.Location = New System.Drawing.Point(327, 41)
+        Me.txtKin.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtKin.Name = "txtKin"
+        Me.txtKin.Size = New System.Drawing.Size(76, 20)
+        Me.txtKin.TabIndex = 4
+        '
+        'txtUinal
+        '
+        Me.txtUinal.Enabled = False
+        Me.txtUinal.Location = New System.Drawing.Point(248, 41)
+        Me.txtUinal.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtUinal.Name = "txtUinal"
+        Me.txtUinal.Size = New System.Drawing.Size(76, 20)
+        Me.txtUinal.TabIndex = 5
+        '
+        'txtTun
+        '
+        Me.txtTun.Enabled = False
+        Me.txtTun.Location = New System.Drawing.Point(168, 41)
+        Me.txtTun.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtTun.Name = "txtTun"
+        Me.txtTun.Size = New System.Drawing.Size(76, 20)
+        Me.txtTun.TabIndex = 6
+        '
+        'txtKatun
+        '
+        Me.txtKatun.Enabled = False
+        Me.txtKatun.Location = New System.Drawing.Point(88, 41)
+        Me.txtKatun.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtKatun.Name = "txtKatun"
+        Me.txtKatun.Size = New System.Drawing.Size(76, 20)
+        Me.txtKatun.TabIndex = 7
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(88, 67)
+        Me.btnConvert.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(234, 25)
+        Me.btnConvert.TabIndex = 8
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(418, 102)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtKatun)
+        Me.Controls.Add(Me.txtTun)
+        Me.Controls.Add(Me.txtUinal)
+        Me.Controls.Add(Me.txtKin)
+        Me.Controls.Add(Me.txtMonth)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtBaktun)
+        Me.Controls.Add(Me.txtDay)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "MainForm"
+        Me.Text = "Julian to Mayan Calendar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDay As System.Windows.Forms.TextBox
+    Friend WithEvents txtBaktun As System.Windows.Forms.TextBox
+    Friend WithEvents txtYear As System.Windows.Forms.TextBox
+    Friend WithEvents txtMonth As System.Windows.Forms.TextBox
+    Friend WithEvents txtKin As System.Windows.Forms.TextBox
+    Friend WithEvents txtUinal As System.Windows.Forms.TextBox
+    Friend WithEvents txtTun As System.Windows.Forms.TextBox
+    Friend WithEvents txtKatun As System.Windows.Forms.TextBox
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+
+End Class
---
+++ MayanCalendar/WindowsApplication1/MainForm.vb
@@ -0,0 +1,185 @@
+﻿Public Class MainForm
+
+    Dim Day, Month, Year, Longform_Julian As Integer
+    Dim Kin, Uinal, Tun, Katun, Baktun, Longform_Mayan As Integer
+    Dim DayChanged, MonthChanged, YearChanged As Boolean
+
+    Sub MainForm_Load() Handles MyBase.Load
+        Day = 1
+        Month = 1
+        Year = 4000
+
+        Longform_Julian = Day + MonthCalculator(Month) + YearCalculator(Year)
+        txtDay.Text = Day
+        txtMonth.Text = Month
+        txtYear.Text = Year
+        MayanFormatter()
+
+    End Sub
+
+
+    Function MonthCalculator(ByVal Input As Integer) As Integer
+
+        Dim Leap, Tyrant As Integer
+
+        Leap = Year Mod 400
+        Leap /= 100
+        If Leap Mod 2 = 0 Then
+            Leap = Leap Mod 4
+        Else
+            Leap = Year Mod 4
+        End If
+
+        If Input > 2 Then
+            If Leap = 0 Then
+                Tyrant = Tyrant + 1
+            End If
+        End If
+
+        If Input >= 0 Then
+            If Input >= 1 Then
+                If Input >= 2 Then
+                    Tyrant = Tyrant + 31
+                    If Input >= 3 Then
+                        Tyrant = Tyrant + 28
+                        If Input >= 4 Then
+                            Tyrant = Tyrant + 31
+                            If Input >= 5 Then
+                                Tyrant = Tyrant + 30
+                                If Input >= 6 Then
+                                    Tyrant = Tyrant + 31
+                                    If Input >= 7 Then
+                                        Tyrant = Tyrant + 30
+                                        If Input >= 8 Then
+                                            Tyrant = Tyrant + 31
+                                            If Input >= 9 Then
+                                                Tyrant = Tyrant + 31
+                                                If Input >= 10 Then
+                                                    Tyrant = Tyrant + 30
+                                                    If Input >= 11 Then
+                                                        Tyrant = Tyrant + 31
+                                                        If Input >= 12 Then
+                                                            Tyrant = Tyrant + 30
+                                                        End If
+                                                    End If
+                                                End If
+                                            End If
+                                        End If
+                                    End If
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+            Return Tyrant
+        End If
+
+        Return 0
+
+    End Function
+
+    Function YearCalculator(ByVal Input As Integer) As Integer
+
+        Dim Value400, Value100, Leaps, Leaper, Result As Integer
+
+        Value400 = Input - Input Mod 400
+        Value400 = Value400 * 365.2425
+
+        Value100 = Input Mod 400
+
+
+        Leaps = Value100 - Value100 Mod 4
+        Leaps = Leaps / 4
+
+        If Value100 > 100 Then
+            Leaper = Value100 - Value100 Mod 100
+            Leaper = Leaper / 100
+            Value100 = Value100 - Leaper
+        End If
+
+        Value100 = (Value100 - 1) * 365
+        Value100 = Value100 + Leaps
+
+        Result = Value400 + Value100
+
+        Return Result
+
+    End Function
+
+    Sub MayanFormatter()
+
+        Dim ConversionHelper As Integer
+
+        Longform_Mayan = Longform_Julian + 1137140
+
+        Baktun = (Longform_Mayan - Longform_Mayan Mod 144000) / 144000
+        ConversionHelper = Longform_Mayan Mod 144000
+        Katun = (ConversionHelper - ConversionHelper Mod 7200) / 7200
+        ConversionHelper = ConversionHelper Mod 7200
+        Tun = (ConversionHelper - ConversionHelper Mod 360) / 360
+        ConversionHelper = ConversionHelper Mod 360
+        Uinal = (ConversionHelper - ConversionHelper Mod 20) / 20
+        ConversionHelper = ConversionHelper Mod 20
+        Kin = ConversionHelper
+
+        txtBaktun.Text = Baktun
+        txtKatun.Text = Katun
+        txtTun.Text = Tun
+        txtUinal.Text = Uinal
+        txtKin.Text = Kin
+
+        Me.Refresh()
+    End Sub
+
+    Sub DayCalculator(ByVal Input As Integer, ByVal Parameter As String)
+
+        Select Case Parameter
+            Case "Day"
+                Longform_Julian = Input + MonthCalculator(Month) + YearCalculator(Year)
+                MayanFormatter()
+            Case "Month"
+                Longform_Julian = Day + MonthCalculator(Input) + YearCalculator(Year)
+                MayanFormatter()
+            Case "Year"
+                Longform_Julian = Day + MonthCalculator(Month) + YearCalculator(Input)
+                MayanFormatter()
+        End Select
+
+    End Sub
+
+
+    Private Sub txtDay_TextChanged(sender As Object, e As EventArgs) Handles txtDay.TextChanged
+        DayChanged = True
+    End Sub
+
+    Private Sub txtMonth_TextChanged(sender As Object, e As EventArgs) Handles txtMonth.TextChanged
+        MonthChanged = True
+    End Sub
+
+    Private Sub txtYear_TextChanged(sender As Object, e As EventArgs) Handles txtYear.TextChanged
+        YearChanged = True
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        If DayChanged = True Then
+            Day = txtDay.Text
+            DayCalculator(Day, "Day")
+            DayChanged = False
+        End If
+
+        If MonthChanged = True Then
+            Month = txtMonth.Text
+            DayCalculator(Month, "Month")
+            MonthChanged = False
+        End If
+
+        If YearChanged = True Then
+            Year = txtYear.Text
+            DayCalculator(Year, "Year")
+            YearChanged = False
+        End If
+
+    End Sub
+
+End Class
